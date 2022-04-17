f09354ed5a06618d174a0b1ffadcc5875442eab2
Aaron Whitfield <aawhitfield@hotmail.com>
2022-04-16 17:19:55 -0700

    doesn't display back button when the scaffold is on a page that is the first route in the navigation stack

--- lib/platform_scaffold.dart
+++ lib/platform_scaffold.dart
@@ -3,25 +3,35 @@ part of 'custom_widgets.dart';
 class CustomPlatformScaffold extends StatelessWidget {
   final Widget child;
   final Widget? title;
-  const CustomPlatformScaffold({Key? key, required this.child, this.title}) : super(key: key);
+  const CustomPlatformScaffold({Key? key, required this.child, this.title})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GetPlatform.isAndroid ? Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: GetPlatform.isAndroid ? const Icon(Icons.arrow_back) : const Icon(CupertinoIcons.chevron_back),
-          onPressed: () => Get.back(),
-        ),
-        title: title,
-      ),
-      body: child,
-    )
+    // from https://stackoverflow.com/questions/58004118/how-to-get-the-number-of-routes-in-navigators-stack
+    bool isFirstNavigationRouteOnStack = (ModalRoute.of(context) != null)
+        ? ModalRoute.of(context)!.isFirst && ModalRoute.of(context)!.isCurrent
+        : false;
+    return GetPlatform.isAndroid
+        ? Scaffold(
+            appBar: AppBar(
+              leading: (isFirstNavigationRouteOnStack)
+                  ? Container()
+                  : IconButton(
+                      icon: GetPlatform.isAndroid
+                          ? const Icon(Icons.arrow_back)
+                          : const Icon(CupertinoIcons.chevron_back),
+                      onPressed: () => Get.back(),
+                    ),
+              title: title,
+            ),
+            body: child,
+          )
         : CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        middle: title,
-      ),
-      child: child,
-    );
+            navigationBar: CupertinoNavigationBar(
+              middle: title,
+            ),
+            child: child,
+          );
   }
 }
